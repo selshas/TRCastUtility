6f03ff9fc022eadd59f4a5a0de18269c3c7c5acf
Selshas Ranos <selshas@gmail.com>
2025-04-24 07:29:33 +0900

    Add FactionPortrait swap function to PlayersNamePanel

--- Assets/Scripts/UtilityApps/PlayersNamePanel/PlayersNamePanel.cs
+++ Assets/Scripts/UtilityApps/PlayersNamePanel/PlayersNamePanel.cs
@@ -1,16 +1,45 @@
 using UnityEngine;
+using UnityEngine.UI;
+using System.Collections.Generic;
 
 public class PlayersNamePanel : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public List<Sprite> MiniPortraitSprites = new List<Sprite>();
+
+    public List<Button> MiniPortraits = new List<Button>();
+
+    public class MiniPortrait : MonoBehaviour
     {
-        
+        public PlayersNamePanel PlayersNamePanel;
+        public int CurrentIndex = 0;
+
+        private Image imgae;
+
+        private void Awake()
+        {
+            imgae = GetComponent<Image>();
+        }
+
+        public void SwapSprite()
+        { 
+            var num_factionPortraits = PlayersNamePanel.MiniPortraitSprites.Count;
+            if (num_factionPortraits == 0)
+                return;
+
+            CurrentIndex = (CurrentIndex + 1) % num_factionPortraits;
+
+            var sprite = PlayersNamePanel.MiniPortraitSprites[CurrentIndex];
+            imgae.sprite = sprite;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Awake()
     {
-        
+        foreach (var button in MiniPortraits)
+        {
+            var miniPortrait = button.gameObject.AddComponent<MiniPortrait>();
+            miniPortrait.PlayersNamePanel = this;
+            button.onClick.AddListener(() => miniPortrait.SwapSprite());
+        }
     }
 }
