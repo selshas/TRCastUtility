be56eeab21a73457a5ce9a4bf1ddb1cf96889730
Selshas Ranos <selshas@gmail.com>
2025-04-24 03:39:29 +0900

    Add ScreenCanvasHelper

---
+++ Assets/Scripts/UtilityApps/ScreenCanvas/ScreenCanvasHelper.cs
@@ -0,0 +1,31 @@
+using System.Linq;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScreenCanvasHelper : MonoBehaviour
+{
+    public ScreenCanvas ScreenCanvas;
+
+    public GameObject PenColorTemplete;
+
+    private void Awake()
+    {
+        var colors = ScreenCanvas.PenColors;
+        var colorNames = colors.Keys.ToArray();
+
+        var parentTransform = PenColorTemplete.transform.parent;
+        for (var i = 0; i < colorNames.Length; i++)
+        {
+            var colorName = colorNames[i];
+            var color = colors[colorName];
+
+            var intance = Instantiate(PenColorTemplete, parentTransform);
+            intance.name = $"{colorName}({i + 1})";
+            intance.GetComponent<RawImage>().color = color;
+            intance.GetComponentInChildren<TextMeshProUGUI>().text = $"{i + 1}";
+        }
+
+        DestroyImmediate(PenColorTemplete);
+    }
+}
