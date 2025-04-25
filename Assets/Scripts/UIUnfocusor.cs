b69186be50b022f13e9b7b514a280e83dc42e381
Selshas Ranos <selshas@gmail.com>
2025-04-25 23:57:31 +0900

    Move location of UIUnfocusor.cs

--- Assets/Scripts/UIUnfocusor.cs
+++ Assets/Scripts/UIUnfocusor.cs
@@ -6,6 +6,8 @@ public class UIUnfocusor : MonoBehaviour
     private void OnApplicationFocus(bool focus)
     {
         if (!focus)
+        {
             EventSystem.current.SetSelectedGameObject(null);
+        }
     }
 }
